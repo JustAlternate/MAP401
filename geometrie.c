6cd77e1ea84a2eb7ddf9bd7c37be1851c617b227
Pascal Isak <pascali@etu.univ-grenoble-alpes.fr>
2023-01-24 15:28:08 +0100

    spécification

--- geometrie.c
+++ geometrie.c
@@ -13,12 +13,14 @@
 //                Initialisation                //
 //////////////////////////////////////////////////
 
+//crée un nouveau vecteur de coordonée (x,y):  V(x,y)
 Vecteur nouveau_vecteur(double x,double y){
     Vecteur v;
     v.x = x;
     v.y = y;
     return v;
 }
+//crée un nouveau point de coordonée (x,y):  P(x,y)
 Point nouveau_point(double x,double y){
     Point p;
     p.x = x;
@@ -29,14 +31,17 @@ Point nouveau_point(double x,double y){
 //                  Conversion                  //
 //////////////////////////////////////////////////
 
+//convertie le point P(x,y) en le vecteur V(x,y)
 Vecteur point_to_vecteur(Point p){
     return nouveau_vecteur(p.x, p.y);
 }
 
+//convertie le vecteur V(x,y) en le point P(x,y)
 Point vecteur_to_point(Vecteur v){
     return nouveau_point(v.x, v.y);
 }
 
+//crée le vecteur allant du point p1 au point p2
 Vecteur couple_point_to_vecteur(Point p1, Point p2){
     return nouveau_vecteur(p2.x - p1.x, p2.y - p1.y);
 }
@@ -45,20 +50,22 @@ Vecteur couple_point_to_vecteur(Point p1, Point p2){
 //                  Operations                  //
 //////////////////////////////////////////////////
 //vecteurs
-
+//renvoie v1+v2
 Vecteur addition_vecteur(Vecteur v1, Vecteur v2){
     return nouveau_vecteur(v1.x + v2.x, v1.y + v2.y);
 }
 
-Vecteur produit_vecteur(double a, Vecteur v){
-    return nouveau_vecteur(v.x * a, v.y * a);
+//renvoie λ*v
+Vecteur produit_vecteur(double λ, Vecteur v){
+    return nouveau_vecteur(λ * v.x, λ * v.y);
 }
 
 //point
+//renvoie p1+p2
 Point addition_Point(Point p1, Point p2){
     return nouveau_point(p1.x + p2.x, p1.y + p2.y);
 }
-
-Point produit_point(double a, Point p){
-    return nouveau_point(a * p.x, a * p.y);
+//renvoie λ*p
+Point produit_point(double λ, Point p){
+    return nouveau_point(λ * p.x, λ * p.y);
 }
