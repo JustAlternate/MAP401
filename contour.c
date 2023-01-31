f88565ce8d5ac46aa84d976f005ec43517cb2422
JustAlternate <lololollololol890@gmail.com>
2023-01-31 04:20:05 +0100

    pushage des test

--- contour.c
+++ contour.c
@@ -10,8 +10,8 @@
 #include "contour.h"
 
 Point trouver_pixel_depart(Image I){
-	for (double i = 1; i<hauteur_image(I);i++){
-		for(double j = 1; j<largeur_image(I);j++){
+	for (double i = 1; i<hauteur_image(I)+1;i++){
+		for(double j = 1; j<largeur_image(I)+1;j++){
 			if (get_pixel_image(I,i,j)){
 				if (!get_pixel_image(I,i,j-1)){
 					Point p = nouveau_point(i,j);
