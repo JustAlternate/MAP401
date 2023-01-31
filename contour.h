346dea75ea74892d03e5d02c3dac0a6e7553ffe1
JustAlternate <lololollololol890@gmail.com>
2023-01-31 03:12:51 +0100

    Entrain de finir la tache 3 hmmmm all cases

--- contour.h
+++ contour.h
@@ -9,5 +9,8 @@ typedef enum {Nord,Sud,Ouest,Est} Orientation;
 
 Point trouver_pixel_depart(Image I);
 
+Point avancer(Point pos, Orientation ori);
+Orientation nouvelle_orientation(Point pos, Orientation ori, Image I);
+Contour recherche_contour(Point depart, Image I);
 
 #endif /* _CONTOUR_H_ */
