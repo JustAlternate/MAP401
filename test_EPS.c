b321d226cdd2227779598a3bb30df62a7759c79b
Pascal Isak <pascali@etu.univ-grenoble-alpes.fr>
2023-02-07 16:44:12 +0100

    test++

--- test_EPS.c
+++ test_EPS.c
@@ -47,6 +47,9 @@ void test_dessiner_ligne(){
     dessiner_ligne(f, 0, 0, 10, 10, 1, 0, 0, 0, 1.5);
     dessiner_ligne(f, 0, 0, 0, 0, 0, 0, 0, 0, 0.);
     fclose(f);
+    FILE *f3 = init_fichier_eps("test_EPS3", 0, 0, 100, 100);
+    dessiner_ligne(f, 100, 0, 0, 100, 1, 0, 1, 1, 1.5);
+    fclose(f3);
 
     FILE *f2 = fopen("test_EPS2.eps", "r");
     if (f2 == NULL){
@@ -59,6 +62,26 @@ void test_dessiner_ligne(){
     fgets(read, 100, f2);
     fgets(read2, 100, f2);
     afficher_resultat_test((0 == strcmp(read, "0 0 moveto 10 10 lineto\n") )&&( 0 == strcmp(read2,"0 0 0 setrgbcolor 1.5 setlinewidth stroke\n")));
+
+    printf("Test 2/3\n");
+    fgets(read, 100, f2);
+    fgets(read2, 100, f2);
+    afficher_resultat_test((0 == strcmp(read, "0 0 moveto 0 0 lineto\n") )&&( 0 == strcmp(read2,"0 0 0 setrgbcolor 0.0 setlinewidth\n")));
+
+    FILE *f4 = fopen("test_EPS3.eps", "r");
+    if (f4 == NULL){
+        afficher_resultat_test( /*false*/ 0);
+    }
+    fgets(read, 100, f4);// les deux 
+    fgets(read2, 100, f4);// premières lignes
+    printf("Test 3/3\n");
+    fgets(read, 100, f4);
+    fgets(read2, 100, f4);
+    afficher_resultat_test((0 == strcmp(read, "100 0 moveto 0 100 lineto\n")) && (0 == strcmp(read2, "0 1 1 setrgbcolor 1.5 setlinewidth stroke\n")));
+}
+
+void test_dessiner_contour(){
+    
 }
 
 int main(int argc, char** argv){
