48d1d1dd3f98826de00fb41273a92f63c95168f1
JustAlternate <lololollololol890@gmail.com>
2023-03-07 16:04:39 +0100

    test_simplification

--- src/test_simplification_contour.c
+++ src/test_simplification_contour.c
@@ -10,13 +10,27 @@
 //foncyions pratiques:
 void afficher_resultat_test(int b){
     if (b){
-        printf(" %s SUCCESS%s\n", ANSI_COLOR_GREEN, RESET_COLOR);
+        printf("%sSUCCESS%s\n", ANSI_COLOR_GREEN, RESET_COLOR);
     }
     else{
-        printf(" %s FAILED%s\n", ANSI_COLOR_RED, RESET_COLOR);
+        printf("%sFAILED%s\n", ANSI_COLOR_RED, RESET_COLOR);
     }
 }
 
 int main(int argc, char** argv){
+
+    if (argc != 7){
+        printf("Usage: ./test_simplification_contour P.x P.y A.x A.y B.x B.y\n");
+        return 1;
+    }
+    Point P = nouveau_point(atof(argv[1]),atof(argv[2]));
+    Point A = nouveau_point(atof(argv[3]),atof(argv[4]));
+    Point B = nouveau_point(atof(argv[5]),atof(argv[6]));
+
+    double resultat = distance_point_segment(P, A, B);
+    printf("distance_point_segment = %f \n",resultat);
+
+
+
     return 0;
 }
