7bd53bbc6103180d85ccc80a1e76124be112e5dd
JustAlternate <lololollololol890@gmail.com>
2023-03-07 15:34:18 +0100

    ajout fichier test pour la fonction distance point segment

---
+++ src/test_simplification_contour.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "geometrie.h"
+#include "types_macros.h"
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define RESET_COLOR        "\x1B[0m"
+
+//foncyions pratiques:
+void afficher_resultat_test(int b){
+    if (b){
+        printf(" %s SUCCESS%s\n", ANSI_COLOR_GREEN, RESET_COLOR);
+    }
+    else{
+        printf(" %s FAILED%s\n", ANSI_COLOR_RED, RESET_COLOR);
+    }
+}
+
+int main(int argc, char** argv){
+    return 0;
+}
