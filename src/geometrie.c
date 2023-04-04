faee471f949b828ba8f8a15a35f172d12f0b8fa3
JustAlternate <lololollololol890@gmail.com>
2023-04-04 14:59:19 +0200

    commençage tache7

--- src/geometrie.c
+++ src/geometrie.c
@@ -6,8 +6,8 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
-#include"geometrie.h"
-
+#include "geometrie.h"
+#include "contour.h"
 
 //////////////////////////////////////////////////
 //                Initialisation                //
@@ -114,3 +114,54 @@ double distance_point_segment(Point P, Point A, Point B){
     //Point Q = nouveau_point(A.x + lambda * (B.x - A.x), A.y * lambda * (B.y - A.y));
     return distance_point(Q,P);
 }
+
+
+//////////////////////////////////////////////////
+//                  Beziers                    //
+//////////////////////////////////////////////////
+
+Bezier2 init_bezier2(Point C0, Point C1, Point C2){
+    Bezier2 bez;
+    bez.C0 = C0;
+    bez.C1 = C1;
+    bez.C2 = C2;
+    return bez;
+}
+
+Bezier3 init_bezier3(Point C0, Point C1, Point C2, Point C3){
+    Bezier3 bez;
+    bez.C0 = C0;
+    bez.C1 = C1;
+    bez.C2 = C2;
+    bez.C3 = C3;
+    return bez;
+}
+
+Point applique_bezier2_point(Bezier2 bez, double x){
+    // On recopie simplement la formule de la forme d'une courbe de bezier.
+    Point a = produit_point((x - 1)*(x - 1), bez.C0);
+    Point b = produit_point(2*(x - 1)*(x), bez.C1);
+    Point c = produit_point((x)*(x), bez.C2);
+    return addition_point(addition_point(a,b),c);
+}
+
+Point applique_bezier3_point(Bezier3 bez, double x){
+    // On recopie simplement la formule de la forme d'une courbe de bezier.
+    Point a = produit_point((x - 1)*(x - 1)*(x-1), bez.C0);
+    Point b = produit_point(3*(x - 1)*(x - 1)*(x), bez.C1);
+    Point c = produit_point(3*(x - 1)*(x)*(x), bez.C2);
+    Point d = produit_point((x)*(x)*(x), bez.C3);
+    return addition_point(addition_point(a,b),addition_point(c,d));
+}
+
+
+
+
+
+
+
+
+
+
+
+
