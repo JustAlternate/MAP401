2ccfa87cce685cd9a24cee2720f632a4bc12f936
JustAlternate <lololollololol890@gmail.com>
2023-04-18 14:37:43 +0200

    lets go la fonction distance_point_bezier2 marcheeeee

--- src/bezier.h
+++ src/bezier.h
@@ -5,7 +5,7 @@
 #ifndef _BEZIER_H_
 #define _BEZIER_H_
 
-#include "sequence_point.h"
+#include "geometrie.h"
 
 //////////////////////////////////////////////////
 //                  Beziers                    //
@@ -27,9 +27,6 @@ typedef struct {
 // Permet de rapidement créer une structure Bezier avec les points.
 Bezier2 init_bezier2(Point C0, Point C1, Point C2);
 Bezier3 init_bezier3(Point C0, Point C1, Point C2, Point C3);
-
-// Permet de transformer un segment en courbe de bezier 2, puisque nous utilisons des liste chainées, on donne la cellule qui correspond au premier point du segment et au dernier, ainsi que le nombre de points pour l'optimisation.
-Bezier2 approx_bezier2(Cellule_Point* depart, Cellule_Point* fin, int nombre_cellule);
 
 // Calcul de la distance entre le point et la courbe (ti represente le numéro du point / nombre total de points du contour simplifiée)
 double distance_point_courbe_bezier2(Point C, Bezier2 bez, double ti);
