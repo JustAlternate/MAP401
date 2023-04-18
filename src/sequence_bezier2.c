d7356669bfcae8b9b77e1581c85a5ac03038c375
Pascal Isak <pascali@etu.univ-grenoble-alpes.fr>
2023-04-18 16:26:08 +0200

    sequence_bezier2 add

---
+++ src/sequence_bezier2.c
@@ -0,0 +1,192 @@
+/****************************************************************************** 
+  Implementation du module sequence_bezier2.c
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "sequence_bezier2.h"
+#include "geometrie.h"
+#include "bezier.h"
+
+//######################//
+//		Bezier2			//
+//######################//
+
+Motif_Bezier2* creer_Motif_Bezier2(){
+	Motif_Bezier2 *L = malloc(sizeof(Motif_Bezier2));
+	L->taille = 0;
+	L->first = NULL;
+	L->last = NULL; 
+	return L;
+}
+Cellule_Motif_Bezier2 *creer_element_Motif_Bezier2(Bezier2 v)
+{
+	Cellule_Motif_Bezier2 *el;
+	el = (Cellule_Motif_Bezier2 *)malloc(sizeof(Cellule_Motif_Bezier2));
+	if (el==NULL)
+	{
+		fprintf(stderr, "creer_element_Motif_Bezier2: allocation impossible\n");
+		exit(-1);
+	}
+	el->val = v;
+	el->suiv = NULL;
+	return el;
+}
+
+void ajouter_element_Motif_Bezier2(Motif_Bezier2 *L, Bezier2 e)
+{
+	Cellule_Motif_Bezier2 *el;
+
+	el = creer_element_Motif_Bezier2(e);
+	if (L->taille == 0)
+	{
+		/* premier �l�ment de la liste */
+		L->first = el;
+		L->last = el;
+	}
+	else
+	{
+		L->last->suiv = el;
+		L->last = el;
+	}
+	L->taille++;
+}
+Motif_Bezier2 supprimer_Motif_Bezier2(Motif_Bezier2 L)
+{
+	Cellule_Motif_Bezier2 *el=L.first;
+	
+	while (el) 
+	{		
+		Cellule_Motif_Bezier2 *suiv=el->suiv;
+		free(el);
+		el = suiv;
+	}
+	L.first = L.last = NULL; L.taille = 0;
+	return L;
+}
+
+/* concat�ne L2 � la suite de L1, renvoie la liste L1 modifi�e */
+Motif_Bezier2 concatener_Motif_Bezier2(Motif_Bezier2 L1, Motif_Bezier2 L2)
+{
+	/* cas o� l'une des deux listes est vide */
+	if (L1.taille == 0) return L2; 
+	if (L2.taille == 0) return L1;
+	
+	/* les deux listes sont non vides */
+	L1.last->suiv = L2.first; /* lien entre L1.last et L2.first */
+	L1.last = L2.last;        /* le dernier �l�ment de L1 est celui de L2 */
+	L1.taille += L2.taille;   /* nouvelle taille pour L1 */
+	return L1;
+}
+
+void print_Motif_Bezier2(Motif_Bezier2 *L){
+	printf("{");
+	Cellule_Motif_Bezier2* cur = L->first;
+	while(cur!=NULL){
+		print_Bezier2(cur->val);
+		cur = cur->suiv;
+		printf("; ");
+	}
+	printf("}\n");
+}
+
+int nombre_Bezier2(Motif_Bezier2 L){
+	//la somme du nombre de Bezier2s dans la liste chainée de Bezier2 L (on ne s'intéresse pas à ce que contiennent les Bezier2s)
+	int nb = 0;
+	Cellule_Motif_Bezier2* cur = L.first;
+	while(cur!=NULL){
+		nb++;
+		cur=cur->suiv;
+	}
+	return nb;
+}
+//#################
+// Dessin_Bezier2//
+//#################
+
+Dessin_Bezier2* creer_Dessin_Bezier2(){
+	Dessin_Bezier2 *L = malloc(sizeof(Dessin_Bezier2));
+	L->taille = 0;
+	L->first = NULL;
+	L->last = NULL; 
+	return L;
+}
+Cellule_Dessin_Bezier2 *creer_element_Dessin_Bezier2(Motif_Bezier2 v)
+{
+	Cellule_Dessin_Bezier2 *el;
+	el = (Cellule_Dessin_Bezier2 *)malloc(sizeof(Cellule_Dessin_Bezier2));
+	if (el==NULL)
+	{
+		fprintf(stderr, "creer_element_Dessin_Bezier2: allocation impossible\n");
+		exit(-1);
+	}
+	el->val = v;
+	el->suiv = NULL;
+	return el;
+}
+
+void ajouter_element_Dessin_Bezier2(Dessin_Bezier2 *L, Motif_Bezier2 e)
+{
+	Cellule_Dessin_Bezier2 *el;
+
+	el = creer_element_Dessin_Bezier2(e);
+	if (L->taille == 0){
+		/* premier �l�ment de la liste */
+		L->first = el;
+		L->last = el;
+	}
+	else{
+		L->last->suiv = el;
+		L->last = el;
+	}
+	L->taille++;
+}
+Dessin_Bezier2 supprimer_Dessin_Bezier2(Dessin_Bezier2 L)
+{
+	Cellule_Dessin_Bezier2 *el=L.first;
+	
+	while (el) {
+		Cellule_Dessin_Bezier2 *suiv=el->suiv;
+		supprimer_Motif_Bezier2(el->val);
+		free(el);
+		el = suiv;
+	}
+	L.first = L.last = NULL; L.taille = 0;
+	return L;
+}
+
+/* concat�ne L2 � la suite de L1, renvoie la liste L1 modifi�e */
+Dessin_Bezier2 concatener_Dessin_Bezier2(Dessin_Bezier2 L1, Dessin_Bezier2 L2)
+{
+	/* cas o� l'une des deux listes est vide */
+	if (L1.taille == 0) return L2; 
+	if (L2.taille == 0) return L1;
+	
+	/* les deux listes sont non vides */
+	L1.last->suiv = L2.first; /* lien entre L1.last et L2.first */
+	L1.last = L2.last;        /* le dernier �l�ment de L1 est celui de L2 */
+	L1.taille += L2.taille;   /* nouvelle taille pour L1 */
+	return L1;
+}
+
+void print_Dessin_Bezier2(Dessin_Bezier2 *L){
+	printf("{");
+	Cellule_Dessin_Bezier2* cur = L->first;
+	while(cur!=NULL){
+		print_Motif_Bezier2(&(cur->val));
+		cur = cur->suiv;
+		printf("; ");
+	}
+	printf("}\n");
+}
+
+int nombre_Motif_dans_Dessin_Bezier2(Dessin_Bezier2 L){
+	//la somme du nombre de Contours dans la liste chainée de Motif_Bezier2 L (on ne s'intéresse pas à ce que contiennent les contours)
+	int nb = 0;
+	Cellule_Dessin_Bezier2* cur = L.first;
+	while(cur!=NULL){
+		nb++;
+		cur=cur->suiv;
+	}
+	return nb;
+}
---
+++ src/sequence_bezier2.h
@@ -0,0 +1,50 @@
+#ifndef _SEQUENCE_BEZIER2_H_
+#define _SEQUENCE_BEZIER2_H_
+
+#include "geometrie.h"
+#include "bezier.h"
+
+typedef struct Cellule_Motif_Bezier2_{
+	Bezier2 val;    /* donn�e de l'�l�ment de liste */
+	struct Cellule_Motif_Bezier2_* suiv; /* pointeur sur l'�l�ment suivant */
+} Cellule_Motif_Bezier2;
+
+typedef struct Motif_Bezier2_{
+	unsigned int taille;        /* nombre d'�l�ments dans la liste */
+	Cellule_Motif_Bezier2 *first; /* pointeur sur le premier �l�ment de la liste */
+	Cellule_Motif_Bezier2 *last;  /* pointeur sur le dernier �l�ment de la liste */
+} Motif_Bezier2;
+
+typedef struct Cellule_Dessin_Bezier2_{
+	Motif_Bezier2 val;    /* donn�e de l'�l�ment de liste */
+	struct Cellule_Dessin_Bezier2_* suiv; /* pointeur sur l'�l�ment suivant */
+} Cellule_Dessin_Bezier2;
+
+typedef struct Dessin_Bezier2_{
+	unsigned int taille;        /* nombre d'�l�ments dans la liste */
+	Cellule_Dessin_Bezier2 *first; /* pointeur sur le premier �l�ment de la liste */
+	Cellule_Dessin_Bezier2 *last;  /* pointeur sur le dernier �l�ment de la liste */
+} Dessin_Bezier2;
+
+
+
+//tout ce que relate des Motif de bezier2 (liste de bezier2)
+Motif_Bezier2* creer_Motif_Bezier2();
+Cellule_Motif_Bezier2 *creer_element_Motif_Bezier2(Bezier2 v);
+void ajouter_element_Motif_Bezier2(Motif_Bezier2* L, Bezier2 e);
+Motif_Bezier2 supprimer_Motif_Bezier2(Motif_Bezier2 L);
+Motif_Bezier2 concatener_Motif_Bezier2(Motif_Bezier2 L1, Motif_Bezier2 L2);
+void print_Motif_Bezier2(Motif_Bezier2 *l);
+int nombre_Bezier2(Motif_Bezier2 L);
+
+// Tout ce qui relate des Dessin Bezier2
+Dessin_Bezier2* creer_Dessin_Bezier2();
+Cellule_Dessin_Bezier2 *creer_element_Dessin_Bezier2(Motif_Bezier2 v);
+void ajouter_element_Dessin_Bezier2(Dessin_Bezier2 *L, Motif_Bezier2 e);
+Dessin_Bezier2 supprimer_Dessin_Bezier2(Dessin_Bezier2 L);
+Dessin_Bezier2 concatener_Dessin_Bezier2(Dessin_Bezier2 L1, Dessin_Bezier2 L2);
+void print_Dessin_Bezier2(Dessin_Bezier2 *L);
+int nombre_Motif_dans_Dessin_Bezier2(Dessin_Bezier2 L);
+
+
+#endif
