cff3f5f64e67cafcd410f20baf8bfc32e63ba9cc
Pascal Isak <pascali@etu.univ-grenoble-alpes.fr>
2023-04-25 14:47:54 +0200

    package de test de simplification bezier

--- src/simplification_bezier2.h
+++ src/simplification_bezier2.h
@@ -2,13 +2,14 @@
 #define _SIMPLIFICATION_BEZIER2_H_
 
 #include "geometrie.h"
+#include "sequence_bezier2.h"
+#include "sequence_point.h"
 #include "bezier.h"
 
 // Permet de transformer un segment en courbe de bezier 2, puisque nous utilisons des liste chainées, on donne la cellule qui correspond au premier point du segment et au dernier, ainsi que le nombre de points pour l'optimisation.
 Bezier2 approx_bezier2(Cellule_Point* depart, Cellule_Point* fin, int nombre_cellule);
 
-Contour simplification_douglas_peucker_Bezier2(Cellule_Point* Pj1, Cellule_Point* Pj2, int d);
-Contour enrobage_douglas_Bezier2(Contour Cont, int d);
-
+Motif_Bezier2 simplification_douglas_peucker_Bezier2(Cellule_Point* Pj1, Cellule_Point* Pj2, double d, int nb_elem);
+Dessin_Bezier2 *simplification_Bezier2(Liste_Contour *LCont, float d);
 
 #endif
