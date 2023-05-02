68ef65b41aaac5e8ab0e171244984ab9bc26970e
Pascal Isak <pascali@etu.univ-grenoble-alpes.fr>
2023-05-02 17:20:01 +0200

    tests de perf

--- src/EPS.h
+++ src/EPS.h
@@ -15,7 +15,8 @@
 char* name_to_name_eps(char* nom_fichier);
 FILE* init_fichier_eps(char* nom_fichier,int xmin, int ymin, int xmax, int ymax);
 void dessiner_ligne(FILE* f, int x_dep,int y_dep,int x_fin, int y_fin, bool style, bool r, bool g, bool b, float width);
-void dessiner_contour(Contour CT, FILE* f, bool r, bool g, bool b, float width, int ymax);
+void enregistrer_liste_contour_vers_EPS(Liste_Contour *LC, char *nom_fichier, int style, int largeur_img, int hauteur_img);
+void dessiner_contour(Contour CT, FILE *f, bool r, bool g, bool b, float width, int ymax);
 void dessiner_motif_Bezier2(FILE* f, Motif_Bezier2 *Mot_bez2, int hauteur_img);
 void enregister_dessin_Bezier2_vers_EPS(Dessin_Bezier2 *des_bez2, char* nom_fichier, int largeur_img, int hauteur_img);
 
