6296c1fdfcbbec64e5d5fabebcef68f34438f27f
Pascal Isak <pascali@etu.univ-grenoble-alpes.fr>
2023-05-03 15:12:50 +0200

    coder mieux c'est bien coder bien c'est mieux

--- script/generation_tache8_part2.sh
+++ script/generation_tache8_part2.sh
@@ -5,15 +5,14 @@ make
 echo "######### segment #########"
 while read ligne
 do
-    nom=$(basename $ligne .pbm)
-    echo -------- $nom --------
-    ../src/test_performance $nom 0 s
-    ../src/test_performance $nom 0.5 s
-    ../src/test_performance $nom 1 s
-    ../src/test_performance $nom 2 s
-    ../src/test_performance $nom 4 s
-    ../src/test_performance $nom 8 s
-    ../src/test_performance $nom 16 s
+    echo -------- $ligne --------
+    ../src/test_performance $ligne 0 s
+    ../src/test_performance $ligne 0.5 s
+    ../src/test_performance $ligne 1 s
+    ../src/test_performance $ligne 2 s
+    ../src/test_performance $ligne 4 s
+    ../src/test_performance $ligne 8 s
+    ../src/test_performance $ligne 16 s
 
 done < "../script/fichiers_performance.txt"
 
@@ -22,14 +21,13 @@ done < "../script/fichiers_performance.txt"
 echo "######### bezier2 #########"
 while read ligne
 do
-    nom=$(basename $ligne .pbm)
-    echo -------- $nom --------
-    ../src/test_performance $nom 0 b
-    ../src/test_performance $nom 0.5 b
-    ../src/test_performance $nom 1 b
-    ../src/test_performance $nom 2 b
-    ../src/test_performance $nom 4 b
-    ../src/test_performance $nom 8 b
-    ../src/test_performance $nom 16 b
+    echo -------- $ligne --------
+    ../src/test_performance $ligne 0 b
+    ../src/test_performance $ligne 0.5 b
+    ../src/test_performance $ligne 1 b
+    ../src/test_performance $ligne 2 b
+    ../src/test_performance $ligne 4 b
+    ../src/test_performance $ligne 8 b
+    ../src/test_performance $ligne 16 b
 
 done < "../script/fichiers_performance.txt"
